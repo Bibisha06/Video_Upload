0f1f3407bfa0db5199e76f25c659d8ef056c2d52
Bibisha06 <bibisha2006z@gmail.com>
2026-08-21 02:32:01 +0530

    reder delay fix

--- web_client/src/layouts/MainLayout.jsx
+++ web_client/src/layouts/MainLayout.jsx
@@ -23,17 +23,14 @@ const MainLayout = () => {
             <Navbar />
             
             <main className="pt-24 pb-10 px-4 md:px-8 max-w-[1600px] mx-auto relative z-10">
-                <AnimatePresence mode="wait">
-                    <motion.div
-                        key={location.pathname}
-                        initial={{ opacity: 0, y: 10 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: -10 }}
-                        transition={{ duration: 0.3 }}
-                    >
-                        <Outlet />
-                    </motion.div>
-                </AnimatePresence>
+                <motion.div
+                    key={location.pathname}
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.3 }}
+                >
+                    <Outlet />
+                </motion.div>
             </main>
         </div>
     );
